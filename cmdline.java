cccbe0d5018c85dc9f318d3d71da8efd56d8c123
Anjali Singh <anjalikumarisingh015@gmail.com>
2022-06-26 16:01:14 +0530

    Display

---
+++ cmdline.java
@@ -0,0 +1,10 @@
+public class cmdline {
+  public static void main(String[] args)  
+  {              
+      int a= Integer.parseInt(args[0]);
+      int b= Integer.parseInt(args[1]);
+      int c= Integer.parseInt(args[2]);
+int sum=a+b+c;
+System.out.println("Sum of three numbers using commandline is = " +sum);
+  }
+}
