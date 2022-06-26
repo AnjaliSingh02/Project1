00b6867328b03431a28f9f1419fcd97e271b86f6
Anjali Singh <anjalikumarisingh015@gmail.com>
2022-06-26 16:15:05 +0530

    First commit

---
+++ Test.java
@@ -0,0 +1,23 @@
+interface A{
+    public void method1();
+}
+class One implements A{
+    public void method1()
+    {
+        System.out.println("Class One method");
+    }
+}
+class Two extends One{
+    public void method1()
+    {
+        System.out.println("Class Two method1");
+    }
+}
+public class Test extends Two{
+    public static void main(String[] args)
+    {
+        A a=new Two();
+        a.method1();
+    }
+    
+}
